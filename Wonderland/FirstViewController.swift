d1908ad5b114e2c099cfda7bb42d36fe198ea1d0
Nil Point <nilpoint@hotmail.com>
2015-09-20 22:08:27 +0800

    Initial Commit

---
+++ Wonderland/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Wonderland
+//
+//  Created by John Alstru on 9/20/15.
+//  Copyright (c) 2015 nilpoint.sample. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
